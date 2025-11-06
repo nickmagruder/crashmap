2bf973e9613b01e531eaa0de8ca30824e1aea18a
_NickMagruder <nick@magruder.info>
2025-11-05 16:23:56 -0800

    Update config

--- crashmap.AppHost/Program.cs
+++ crashmap.AppHost/Program.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.crashmap_Server>("crashmap-server");
+var server = builder.AddProject<Projects.crashmap_Server>("crashmap-server");
+
+builder.AddNpmApp("crashmap-client", "../crashmap.client", "dev")
+    .WithReference(server)
+    .WithHttpEndpoint(env: "SERVER_URL")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
